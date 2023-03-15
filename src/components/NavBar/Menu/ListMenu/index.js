1c1db5b08a529763de5c2b5b5875c772ff1b8136
Chabaard <romainchabard@gmail.com>
2023-03-15 19:28:20 +0100

    add random video mod

--- src/components/NavBar/Menu/ListMenu/index.js
+++ src/components/NavBar/Menu/ListMenu/index.js
@@ -1,14 +1,28 @@
 /* eslint-disable max-len */
 /* eslint-disable brace-style */
-import React, { useEffect } from 'react';
+import React, { useContext, useEffect } from 'react';
 
 import PropTypes from 'prop-types';
-import { RiFilmLine, RiHomeLine, RiListSettingsFill, RiStarLine } from 'react-icons/ri';
+import { RiFilmLine, RiHomeLine, RiListSettingsFill, RiShuffleFill, RiStarLine } from 'react-icons/ri';
 import { useNavigate } from 'react-router-dom';
 import './styles.scss';
+import { VideoContext } from '../../../../context/VideoContext';
 
 function ListMenu({ setClickAway, setVisible, menuRef }) {
   const navigate = useNavigate();
+  const { getRandomVideo } = useContext(VideoContext);
+
+  const navToRandomVideo = async () => {
+    const randomVideo = await getRandomVideo();
+    if (randomVideo) {
+      if (randomVideo.type === "movies") {  
+        navigate(`/play/${randomVideo.type}/${randomVideo.movie.id}`);
+      } else if (randomVideo.type === "series") {  
+        navigate(`/play/${randomVideo.type}/${randomVideo.series.id}/0`);
+      }
+      setVisible(false);
+    }
+  }
 
   useEffect(() => {
     const onPointerUp = (event) => {
@@ -32,6 +46,10 @@ function ListMenu({ setClickAway, setVisible, menuRef }) {
       <div className="link" onClick={() => { navigate('/series'); setVisible(false); }}>
         <RiFilmLine />
         <h2>Séries</h2>
+      </div>
+      <div className="link" onClick={navToRandomVideo}>
+        <RiShuffleFill />
+        <h2>Aléatoire</h2>
       </div>
       <div className="link" onClick={() => { navigate('/favoris'); setVisible(false); }}>
         <RiStarLine />
